65b493f5a8fd484f87fc7f6e2e0adee30c47d4d9
Stanislav <stgolovanov@gmail.com>
2012-04-15 19:47:23 +0400

    import methods comments wip

--- history UI/src/org/history/ImportHistory.java
+++ history UI/src/org/history/ImportHistory.java
@@ -21,6 +21,7 @@ public class ImportHistory {
 	 * @throws IOException 
 	 */
 	
+	// concat two byte arrays
 	static byte[] concat(byte[] A, byte[] B) {
 		byte[] C= new byte[A.length+B.length];
 		   System.arraycopy(A, 0, C, 0, A.length);
@@ -31,6 +32,8 @@ public class ImportHistory {
 	
 	static void makeCacheFile(DataOutputStream localDataOutputStream, String myuin) {
 	    try {
+	    	// write useless history item to .cache, it shows in main chat window
+	    	// TODO rework this to get proper message from contact ( prefer last one )
 			localDataOutputStream.writeInt(26);
 			localDataOutputStream.writeByte(0);
 			localDataOutputStream.writeBoolean(false);
@@ -48,20 +51,21 @@ public class ImportHistory {
 	}
 
 	public boolean importHistory(String myuin, String dir) throws IOException {
-		// TODO Auto-generated method stub
-		//String myuin;
+
 		String contactuin = "";
 		
 		File importDir = new File(dir);
 		
+		// make output file in dir with history files
 	    File outputFile = new File(importDir + "\\" + myuin + ".jpha");
+	    
 	    DataOutputStream localDataOutputStream = new DataOutputStream(new FileOutputStream(outputFile));
 	    
-	    
+	    // write header and users uin
 		localDataOutputStream.writeUTF("JPHA");
 		localDataOutputStream.writeUTF(myuin);
 		
-
+		// get files in directory
 		String[] children = importDir.list();
 		
 		if (children == null) {
@@ -71,19 +75,24 @@ public class ImportHistory {
 		        // Get filename of file or directory
 		        String filename = children[i];
 		        
+		        // if there already output file - skip it
 		        if (filename.equals(myuin + ".jpha"))
 		        {
 		        	continue;
 		        }
 		        
+		        // get contact uin - strip extension of filename
 		        contactuin = filename.substring(0, filename.length() - 4);
 		        
 		        InputStream localFile = new FileInputStream(importDir + "\\" +  filename);
 		        
+		        
 				localDataOutputStream.writeUTF(contactuin);		
 				
+				// write cache to jpha file
 				makeCacheFile(localDataOutputStream, myuin);
 				
+				// make a temp file for history file ( to get proper history length )
 			    File tempfile = File.createTempFile(contactuin, ".hst");
 			    
 			    tempfile.deleteOnExit();
@@ -91,13 +100,15 @@ public class ImportHistory {
 			    DataOutputStream tempOutStream = new DataOutputStream(new FileOutputStream(tempfile));
 			    
 			    try {
+			    	// write proper history to temp file
 					makeHstFile(myuin, contactuin, localFile, tempOutStream);
 				} catch (ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 					return false;
 				}
-			    			    
+			    
+			    // write temp file to jpha file
 			    readTempFile(tempfile, localDataOutputStream);
 		    }
 		}
@@ -108,12 +119,15 @@ public class ImportHistory {
 	public static void readTempFile(File tempfile,
 			DataOutputStream localDataOutputStream) throws IOException,
 			FileNotFoundException {
+		
 		byte[] buf = new byte[1024];
-	    
+		
+		// write filesize to jpha file
 	    localDataOutputStream.writeInt((int) (tempfile.length()));
 	    
 	    InputStream tempread = new FileInputStream(tempfile);
-
+	    
+	    // copy contents
 	    int len;
 	    while ((len = tempread.read(buf)) > 0){
 	    	localDataOutputStream.write(buf, 0, len);
@@ -124,6 +138,8 @@ public class ImportHistory {
 	public static void makeHstFile(String myuin, String contactuin,
 			InputStream localFile, DataOutputStream localDataOutputStream) throws ParseException {
 		BufferedReader localDataInputStream;
+		
+		// read local .txt history file
 		localDataInputStream = new BufferedReader(new InputStreamReader(localFile));
 		  try {
 			byte[] strbytes;
@@ -133,19 +149,31 @@ public class ImportHistory {
 			Date timestamp = null;
 			byte direction = 0;
 			boolean firstline = true;
+			
 			DateFormat df = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+			
+			// 0d0a0d0a byte string - DOS new string ( needed to correctly parse multi-lined messages )
 			byte[] newlinebytes = new byte[] { 0x0D, 0x0A, 0x0D, 0x0A };
+			
 			while (localDataInputStream.ready())
 			  {
+				// this cycle is quite weird to understand O_o
+				// TODO refactor and optimize this shit
+				
+				 if string is empty - read new line
 				if (str == "")
 				{
 				str = localDataInputStream.readLine();
 				}
+				
+				// str2 contains next line - needed to correctly read multilined messages
 				else 
 				{
 					str = str2;
 				}
-				if ( str2.startsWith(myuin))
+				
+				// if str2 starts with 
+				if (str2.startsWith(myuin))
 				{
 					localDataOutputStream.writeByte(direction);
 					localDataOutputStream.writeBoolean(false);
@@ -161,13 +189,15 @@ public class ImportHistory {
 					localDataOutputStream.writeByte(direction);
 					localDataOutputStream.writeBoolean(false);
 					if (timestamp == null)
+						
 					{
 					timestamp = df.parse(str2.substring(myuin.length()+2, str2.length() -1));
 					}
+					
 					localDataOutputStream.writeLong(timestamp.getTime());
 				}
 
-				if ( str.startsWith(myuin))
+				if (str.startsWith(myuin))
 				{
 					localDataOutputStream.writeByte(0);
 					localDataOutputStream.writeBoolean(false);
@@ -204,68 +234,88 @@ public class ImportHistory {
 
 				else
 				{
+					// if string doesnt start with uin - it's usually a direct message
+					
 					strlen = str.length();
+					
 					if (str != "")
 					{
+						// read next line - to know whether this is a multiline message or just single line )
+						str2 = localDataInputStream.readLine();
 
-					str2 = localDataInputStream.readLine();
-					
-					System.out.println(str);
-					System.out.println(str2);
-					if (str2 == null)
-					{
-						strbytes = str.getBytes();
-						localDataOutputStream.writeInt(strlen);
-						localDataOutputStream.write(strbytes);
-						continue;
-					}
-					
-					if ( str2.startsWith(myuin))
-					{
-						try {
-							direction = 0;
-							timestamp = df.parse(str2.substring(myuin.length()+2, str2.length() -1));
-						} catch (ParseException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
+						// debug
+						System.out.println(str);
+						System.out.println(str2);
 						
-					}
-					else if (str2.startsWith(contactuin))
-					{
-						try {
-							direction = 1;
-							timestamp = df.parse(str2.substring(contactuin.length()+2, str2.length() -1));
-						} catch (ParseException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-
-					while (true)
-					{	
+						// if str2 = null -> EOF - write all info to output stream
 						if (str2 == null)
 						{
-							break;
+							strbytes = str.getBytes();
+							localDataOutputStream.writeInt(strlen);
+							localDataOutputStream.write(strbytes);
+							continue;
 						}
-						if (str2.startsWith(myuin) || str2.startsWith(contactuin))
+
+						// if str2 starts with uin - store direction and timestamp in global variables because its a new message and we need to process it later )
+						if ( str2.startsWith(myuin))
 						{
-							break;
+							try {
+								direction = 0;
+								timestamp = df.parse(str2.substring(myuin.length()+2, str2.length() -1));
+							} catch (ParseException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+
 						}
-						if (str2.length() == 0)
+						
+						// if str2 starts with uin - store direction and timestamp in global variables because its a new message and we need to process it later )
+						else if (str2.startsWith(contactuin))
 						{
-							str = new String(concat(str.getBytes(),newlinebytes));
-							strlen += 4;
+							try {
+								direction = 1;
+								timestamp = df.parse(str2.substring(contactuin.length()+2, str2.length() -1));
+							} catch (ParseException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 						}
-						else
-						{
-						str += str2;
-						strlen += str2.length();
+
+						// cycle to read multiline messages
+						while (true)
+						{	
+							// EOF - stop
+							if (str2 == null)
+							{
+								break;
+							}
+							
+							// if str2 start with uin - stop
+							if (str2.startsWith(myuin) || str2.startsWith(contactuin))
+							{
+								break;
+							}
+							
+							// if length = 0 - DOS new line ( 0d0a0d0a ). Concat str byte array and 0d0a0d0a byte[], increment string length
+							if (str2.length() == 0)
+							{
+								str = new String(concat(str.getBytes(),newlinebytes));
+								strlen += 4;
+							}
+							
+							// usual case - append second line to first and increment length of string
+							else
+							{
+								str += str2;
+								strlen += str2.length();
+							}
+							
+							// read line for next iteration
+							str2 = localDataInputStream.readLine();
 						}
-						str2 = localDataInputStream.readLine();
-					}
 					}
 					
+					// write str length and byte array to output stream
 					strbytes = str.getBytes();
 					localDataOutputStream.writeInt(strlen);
 					localDataOutputStream.write(strbytes);
@@ -273,11 +323,14 @@ public class ImportHistory {
 
 				}
 				
+				// set string to "" , forgot why O_o
+				// TODO what happens here?
 				str = "";
 
 			  }
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			History.showMessage("Something went wrong, import failed");
 			e.printStackTrace();
 		}
 	}
